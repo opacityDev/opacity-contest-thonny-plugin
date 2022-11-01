6b500e6948fc966c92de8de32fe7a236add5024f
opacityDev <mohamed.hassan@opacity.dev>
2022-11-01 02:54:13 +0100

    Editor event discovered

--- opacity_share/lib/watcher.py
+++ opacity_share/lib/watcher.py
@@ -7,10 +7,15 @@ class Watcher():
     RELEASED=2
 
 
-    def __init__(self,editor):
+    def __init__(self):
+        pass
+
+    def set_editor_notebook(self,editor_notebook):
+        editor = editor_notebook.get_current_editor()
         self.codeview = editor.get_code_view()
         self.text = editor.get_text_widget()
         self.editor = editor
+        self.editor_notebook = editor_notebook
         # list to store keycode of currently pressed keys
         self.keys_stat = dict()
         self._controls = list()
@@ -18,6 +23,18 @@ class Watcher():
         self.text.bind("<KeyPress>",self.key_pressed,True)
         self.text.bind("<KeyRelease>",self.key_released,True)
         self._releasing = False 
+        def test(selfo,*args):
+            if not hasattr(self,"second_editor"):
+                return
+
+            print("a7a " + f'{args}')
+            self.second_editor.get_code_view()._vertical_scrollbar_update(selfo,args)
+            editor.get_code_view()._vertical_scrollbar_update(selfo,args)
+        self.text["yscrollcommand"]=test
+
+    def get_second_editor(self):
+        self.second_editor=self.editor_notebook.get_current_editor()    
+
     def key_pressed(self,k):
         
         if k.keycode not in self.keys_stat:
@@ -29,20 +46,37 @@ class Watcher():
             self.keys_stat[k.keycode]["stat"] = Watcher.PRESSED
             self.keys_stat[k.keycode]["last_pressed"] = time.time()
 
+        for control in self._controls:
+            control.check(self,k) 
         index = self.text.index("insert")
         if index and "." in index:
                 line, col = index.split(".")    
                 print(("Key [{}] [{}] pressed @  {} : {}".format(k.char,k.keycode,line, int(col) + 1)))
         
+    def _on_copy(self):
+        pass
 
+    def _on_paste(self):
+        pass
+
+    def _on_cut(self):
+        pass
+
+    def _on_tab(self):
+        pass
+
+    def _on_selection(self):
+        pass
 
     def _define_controls(self):
         # on Copy action
         def _on_copy(self):
             tk.messagebox.showinfo(title=None,message="You copied sth i see you")
-        def on_myster(self):
+        def _on_myster(self):
             tk.messagebox.showinfo(title=None,message="You did run the myster sth i see you")
-        self.add_control(Watcher.Combo(_on_copy,37,58))
+        self.add_control(Watcher.Combo(_on_myster,37,58))
+        self.add_control(Watcher.Combo(_on_copy,37,55))
+
     def key_released(self,event):
  
         index = self.text.index("insert")
@@ -59,12 +93,10 @@ class Watcher():
             self.keys_stat[event.keycode]["stat"] = Watcher.RELEASED
             self.keys_stat[event.keycode]["last_released"] = time.time()
              
-        for control in self._controls:
-            control.check(self,event ) 
-         
     def add_control(self,control):
         self._controls.append(control)
     
+
     class Combo():
 
         TIMEGAP = 0.09
@@ -87,9 +119,12 @@ class Watcher():
                 if (not is_pressed):
                     return
                 if (i!=0):
-                    prev_key_stat = watcher.keys_stat[self.keys[i-1]]
+                    prev_key = self.keys[i-1]
+                    prev_key_stat = watcher.keys_stat[prev_key] 
                     if (key_stat['last_pressed'] < prev_key_stat['last_pressed']):
                         return
+                  
+                    
             print(key_stat['last_released'],prev_key_stat['last_released'])
             self.action(watcher)
 
--- thonnycontrib/opacity_share_loader.py
+++ thonnycontrib/opacity_share_loader.py
@@ -3,19 +3,23 @@ from tkinter import Label, Text, Toplevel
 import tkinter as tk
 from thonny import get_workbench 
 from opacity_share.lib.watcher import Watcher
-
+w1=Watcher()
 def param():
     editor_notebook = get_workbench().get_editor_notebook() 
-    w1 = Watcher(editor_notebook.get_current_editor())
-    pass  
+    w1.set_editor_notebook(editor_notebook)
  
-
+def set_second_editor():
+    w1.get_second_editor()
  
 def load_plugin():
     get_workbench().add_command(command_id="opacity_contests_menu",
                                 menu_name="tools",
                                 command_label="OC Profile",
                                 handler=param)
+    get_workbench().add_command(command_id="opacity_second_editor",
+                                menu_name="tools",
+                                command_label="Set second editor",
+                                handler=set_second_editor)
     pass
 
 
